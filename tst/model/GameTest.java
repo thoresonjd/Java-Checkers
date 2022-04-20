4ee8f247e244beecbc6aefbe54b7b009d850afcc
Francis Kogge <koggefa@gmail.com>
2022-04-20 13:20:05 -0700

    Added unit tests for copy, restart, and setGameState methods in GameTest.java

--- tst/model/GameTest.java
+++ tst/model/GameTest.java
@@ -2,6 +2,13 @@ package model;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,6 +16,85 @@ class GameTest {
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    void copy_shouldReturnCopy() {
+        Game game = new Game();
+        Game gameCopy = game.copy();
+
+        assertNotEquals(game, gameCopy);
+    }
+
+    @Test
+    void restart_shouldResetGameState() {
+        Game game = new Game(new Board(), true, -1);
+        String initialState = game.getGameState();
+        game.setGameState("6666666666660000000044444444444410");
+
+        game.restart();
+
+        assertEquals(initialState, game.getGameState());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "000000000000000000000000000000001",      // On boundary:         exactly 33 characters
+            "0000000000000000000000000000000010",     // Just above boundary: 34 characters
+            "0000000000000000000000000000000010000"}) // Above boundary:      37 characters
+    void setGameState_shouldSetP1TurnToTrue_whenStateLengthIsGreaterThan32AndChar32Is1(String state) {
+        Game game = new Game(new Board(), true, -1);
+        game.setGameState(state);
+        System.out.println(game.getGameState());
+
+        assertTrue(game.isP1Turn());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "000000000000000000000000000000000",
+            "0000000000000000000000000000000000",
+            "0000000000000000000000000000000000000"})
+    void setGameState_shouldSetP1TurnToFalse_whenStateLengthIsGreaterThan32AndChar32IsNot1(String state) {
+        Game game = new Game(new Board(), true, -1);
+        game.setGameState(state);
+        System.out.println(game.getGameState());
+
+        assertFalse(game.isP1Turn());
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void setGameState_shouldSetSkipToIntegerStartingAtIndex33_whenIndex33IsAnInteger(String state, int expectedResult) {
+        Game game = new Game(new Board(), true, -1);
+        game.setGameState(state);
+
+        assertEquals(expectedResult, game.getSkipIndex());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "000000000000000000000000000000000x",
+            "000000000000000000000000000000000x",
+            "000000000000000000000000000000000x000"})
+    void setGameState_shouldSetSkipIndexToNegativeOne_whenIndex33IsNotAnInteger(String state) {
+        Game game = new Game(new Board(), true, -1);
+        game.setGameState(state);
+
+        assertEquals(-1, game.getSkipIndex());
+    }
+
+    /*** Methods for providing arguments to parameterized tests ***/
+
+    private static Stream<Arguments> setGameState_shouldSetSkipToIntegerStartingAtIndex33_whenIndex33IsAnInteger() {
+        return Stream.of(
+                Arguments.of("0000000000000000000000000000000000", 0),
+                Arguments.of("0000000000000000000000000000000001", 1),
+                Arguments.of("0000000000000000000000000000000005", 5),
+                Arguments.of("0000000000000000000000000000000009", 9),
+                Arguments.of("000000000000000000000000000000000-1", -1),
+                Arguments.of("0000000000000000000000000000000001000", 1000)
+        );
     }
 
     @AfterEach
