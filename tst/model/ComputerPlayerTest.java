be8ac75e352d7cb37131338f5d3855e2ea3a2ae6
Justin Thoreson <thoresonjd@gmail.com>
2022-04-23 19:37:24 -0700

    ComputerPlayer tests started

--- tst/model/ComputerPlayerTest.java
+++ tst/model/ComputerPlayerTest.java
@@ -2,6 +2,9 @@ package model;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +16,56 @@ class ComputerPlayerTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    /*** Update Game ***/
+
+    @Test
+    void updateGame_DoesNothing_WhenGameIsNull() {
+        Player player = new ComputerPlayer();
+        player.updateGame(null);
+    }
+
+    @Test
+    void updateGame_ExpectBoardChange_WhenComputerMakesMove() {
+        Game game = new Game();
+        Player player = new ComputerPlayer();
+        String initialBoard = game.getBoard().toString();
+
+        // Computer makes move
+        player.updateGame(game);
+
+        String boardAfterMove = game.getBoard().toString();
+        assertFalse(initialBoard.equals(boardAfterMove));
+    }
+
+    @Test
+    void updateGame_ExpectNoBoardChange_WhenSkipIndexIsZero() {
+        Game game = new Game(null, true, 0);
+        Player player = new ComputerPlayer();
+        String initialBoard = game.getBoard().toString();
+
+        // Computer makes move
+        player.updateGame(game);
+
+        String boardAfterMove = game.getBoard().toString();
+        assertTrue(initialBoard.equals(boardAfterMove));
+    }
+
+    @Test
+    void updateGame_ExpectNoBoardChange_WhenPlayerOneTurnIsFalse() {
+        Game game = new Game();
+        Player player = new ComputerPlayer();
+        String initialBoard = game.getBoard().toString();
+
+        // Set player one turn to false;
+        game.setP1Turn(false);
+        assertFalse(game.isP1Turn());
+
+        // Computer makes move
+        player.updateGame(game);
+
+        String boardAfterMove = game.getBoard().toString();
+        assertFalse(initialBoard.equals(boardAfterMove));
     }
 }
