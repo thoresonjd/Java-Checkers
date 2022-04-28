f0081bb20c30b4d0a6c5cf6b55171e07be55db06
ChungNYCU <79520786+ChungNYCU@users.noreply.github.com>
2022-04-28 01:14:15 -0700

    Update SessionTest.java with 100% statement coverage

--- tst/network/SessionTest.java
+++ tst/network/SessionTest.java
@@ -2,6 +2,9 @@ package network;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.Socket;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,4 +17,104 @@ class SessionTest {
     @AfterEach
     void tearDown() {
     }
+
+    /*** constructor ***/
+
+    @Test
+    void Session_CreateObject_WhenCorrectTriggered() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session1 = new Session(connectionListener, "", "", 0);
+        Session session2 = new Session("",0,"",0);
+    }
+
+    /*** getListener ***/
+
+    @Test
+    void getListener_ReturnListener_WhenCorrectTriggered() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session = new Session(connectionListener, "", "", 0);
+        session.getListener();
+    }
+
+    /*** setListener ***/
+
+    @Test
+    void setListener_SetListenerObject_WhenCorrectInput() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session = new Session(connectionListener, "", "", 0);
+        session.setListener(connectionListener);
+    }
+
+    /*** getSid ***/
+
+    @Test
+    void getSid_ReturnSid_WhenCorrectTriggered() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session = new Session(connectionListener, "", "", 0);
+        session.getSid();
+    }
+
+    /*** setSid ***/
+
+    @Test
+    void setSid_SetSidValue_WhenCorrectInput() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session = new Session(connectionListener, "", "", 0);
+        session.setSid("");
+    }
+
+    /*** getDestinationHost ***/
+
+    @Test
+    void getDestinationHost_GetDestinationHost_WhenCorrectTriggered() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session = new Session(connectionListener, "", "", 0);
+        session.getDestinationHost();
+    }
+
+    /*** setDestinationHost ***/
+
+    @Test
+    void setDestinationHost_GetDestinationHost_WhenCorrectInput() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session = new Session(connectionListener, "", "", 0);
+        session.setDestinationHost("");
+    }
+
+    /*** getDestinationPort ***/
+
+    @Test
+    void getDestinationPort_GetDestinationPort_WhenCorrectTriggered() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session = new Session(connectionListener, "", "", 0);
+        session.getDestinationPort();
+    }
+
+    /*** setDestinationPort ***/
+
+    @Test
+    void setDestinationPort_SetDestinationPort_WhenCorrectInput() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session = new Session(connectionListener, "", "", 0);
+        session.setDestinationPort(0);
+    }
+
+    /*** getSourcePort ***/
+
+    @Test
+    void getSourcePort_GetSourcePort_WhenCorrectTriggered() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session = new Session(connectionListener, "", "", 0);
+        session.getSourcePort();
+    }
+
+    /*** setSourcePort ***/
+
+    @Test
+    void setSourcePort_SetSourcePort_WhenCorrectInput() {
+        ConnectionListener connectionListener = new ConnectionListener();
+        Session session = new Session(connectionListener, "", "", 0);
+        session.setSourcePort(0);
+    }
+
 }
