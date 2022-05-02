0bb109cea87be9d4ae2d6f93d9f15f3898e8afd7
Justin Thoreson <thoresonjd@gmail.com>
2022-05-02 16:04:49 -0700

    Exercise test in CheckerBoardTest

--- tst/network/CheckersNetworkHandlerTest.java
+++ tst/network/CheckersNetworkHandlerTest.java
@@ -1,9 +1,16 @@
 package network;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import ui.CheckersWindow;
+import ui.CheckerBoard;
+import ui.OptionPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 class CheckersNetworkHandlerTest {
 
@@ -14,4 +21,18 @@ class CheckersNetworkHandlerTest {
     @AfterEach
     void tearDown() {
     }
+
+//    /*** Action Performed ***/
+//
+//    @Test
+//    void actionPerformed() {
+//        CheckersWindow window = new CheckersWindow();
+//        CheckerBoard board = new CheckerBoard(window);
+//        OptionPanel panel = new OptionPanel(window);
+//        CheckersNetworkHandler handler = new CheckersNetworkHandler(
+//            true, window, board, panel
+//        );
+//
+//        handler.actionPerformed(new ActionEvent(0, 0, ""));
+//    }
 }
--- tst/ui/CheckerBoardTest.java
+++ tst/ui/CheckerBoardTest.java
@@ -1,9 +1,13 @@
 package ui;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import model.NetworkPlayer;
+import model.Player;
+import model.Game;
 
 class CheckerBoardTest {
 
@@ -13,5 +17,17 @@ class CheckerBoardTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    /*** updateGame ***/
+
+    @Test
+    void updateGame_Exercise() {
+        Player player1 = new NetworkPlayer();
+        Player player2 = new NetworkPlayer();
+        CheckersWindow window = new CheckersWindow();
+        Game game = new Game();
+        CheckerBoard board = new CheckerBoard(window, game, player1, player2);
+        board.updateNetwork();
     }
 }
