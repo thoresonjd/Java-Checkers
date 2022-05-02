fcc937cc415339367caaa597bac0bfc81fdb9397
Justin Thoreson <thoresonjd@gmail.com>
2022-05-01 20:10:21 -0700

    MoveGeneratorTest getMoves when board is null

--- tst/logic/MoveGeneratorTest.java
+++ tst/logic/MoveGeneratorTest.java
@@ -1,27 +1,30 @@
 package logic;
 
 import model.Board;
-import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import java.awt.Point;
 import java.util.List;
-import java.util.stream.Stream;
 
 class MoveGeneratorTest {
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
     }
 
-    @org.junit.jupiter.api.AfterEach
+    @AfterEach
     void tearDown() {
+    }
+
+    /*** Get Moves ***/
+
+    @Test
+    void getMoves_ExpectEmptyList_WhenBoardIsNull() {
+        List<Point> moves = MoveGenerator.getMoves(null, null);
+        assertTrue(moves.isEmpty());
     }
 }
