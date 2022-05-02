4a06b8b64566fee8fbd8e5f301a1633c463d59a5
Justin Thoreson <thoresonjd@gmail.com>
2022-05-01 19:44:55 -0700

    ConnectionHandlerTest getListener() tested for valid ports

--- tst/network/ConnectionHandlerTest.java
+++ tst/network/ConnectionHandlerTest.java
@@ -2,6 +2,11 @@ package network;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.net.Socket;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,4 +19,16 @@ class ConnectionHandlerTest {
     @AfterEach
     void tearDown() {
     }
+
+    /*** Get Listener ***/
+
+    @ParameterizedTest
+    @CsvSource({"1", "2", "3", "4000"})
+    void getListener_ExpectProvidedPortFromListener_WhenPortIsValid(int port) {
+        ConnectionHandler handler = new ConnectionHandler(new ConnectionListener(port), new Socket());
+        ConnectionListener listener = handler.getListener();
+        assertEquals(port, listener.getPort());
+    }
+
+
 }
