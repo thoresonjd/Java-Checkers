79319383fbf61fa9d515d8d4137dc1c1c5b365fe
Justin Thoreson <thoresonjd@gmail.com>
2022-05-02 17:29:14 -0700

    CheckerBoardTest setLightTile expect white tile when tile is null

--- tst/ui/CheckerBoardTest.java
+++ tst/ui/CheckerBoardTest.java
@@ -11,6 +11,8 @@ import model.NetworkPlayer;
 import model.Player;
 import model.Game;
 
+import java.awt.*;
+
 class CheckerBoardTest {
 
     @BeforeEach
@@ -34,10 +36,42 @@ class CheckerBoardTest {
         // Black checker = 6, empty tile = 0, white checker = 4
         String initialState = "666666666666000000004444444444441-1";
         String newState = "444444444444000000006666666666661-1";
-        boolean success = board.setGameState(true, newState, initialState);
+        boolean success = board.setGameState(false, newState, initialState);
 
         assertTrue(success);
         assertEquals(newState, game.getGameState());
+    }
+
+    @Test
+    void setGameState_ExpectFailure_WhenInitialStateIsNotExpectedState() {
+        Player player1 = new NetworkPlayer();
+        Player player2 = new NetworkPlayer();
+        CheckersWindow window = new CheckersWindow();
+        Game game = new Game();
+        CheckerBoard board = new CheckerBoard(window, game, player1, player2);
+
+        // Black checker = 6, empty tile = 0, white checker = 4
+        String initialState = "444444444444000000006666666666661-1";
+        String newState = "666666666666000000004444444444441-1";
+        assertFalse(board.setGameState(true, newState, initialState));
+    }
+
+    /*** setLightTile ***/
+
+    @Test
+    void setLightTile_ExpectWhite_WhenLightTileIsNull() {
+        // Set up
+        Player player1 = new NetworkPlayer();
+        Player player2 = new NetworkPlayer();
+        CheckersWindow window = new CheckersWindow();
+        Game game = new Game();
+        CheckerBoard board = new CheckerBoard(window, game, player1, player2);
+
+        // Set light tile
+        board.setLightTile(null);
+        Color expected = Color.WHITE;
+        Color actual = board.getLightTile();
+        assertEquals(expected, actual);
     }
 
     /*** update ***/
